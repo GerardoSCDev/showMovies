d421ec9bbc572e82d5316cd6887c3dc25d043a7f
Gerardo SC <gerardo.sc.dev@gmail.com>
2025-03-15 18:55:56 -0600

    Initial Commit

---
+++ showMovies/showMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  showMoviesApp.swift
+//  showMovies
+//
+//  Created by Gerardo Santillan on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct showMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
